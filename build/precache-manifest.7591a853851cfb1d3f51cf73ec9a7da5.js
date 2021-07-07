21a09733cb9fc887f75c4336e95b57d8c79b9138
MrbTd <mrbtd2000@gmail.com>
2021-07-07 00:08:28 +0000

    first commit

---
+++ build/precache-manifest.7591a853851cfb1d3f51cf73ec9a7da5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e8e9060649ec46ed33d1b288df58e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fca7e8440e8b3f61c5a",
+    "url": "/static/js/2.8d8f4284.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d8f4284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dcffbaf53530aa65a37",
+    "url": "/static/js/main.e376f1ce.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,94 @@
+const express=require('express')
+const app=express()
+const cors = require('cors')
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+let Persons=[
+    { 
+        id: 1,
+        name: "Arto Hellas", 
+        number: "040-123456",
+    },
+    { 
+        id: 2,
+        name: "Ada Lovelace", 
+        number: "39-44-5323523",
+    },
+    { 
+        id: 3,
+        name: "Dan Abramov", 
+        number: "12-43-234345",
+    },
+    { 
+        id: 4,
+        name: "Mary Poppendieck", 
+        number: "39-23-6423122",
+    }
+  ]
+
+  function getRandomInt() {
+    return Math.floor(Math.random() * 1000);
+  }
+  
+  app.get('/',(req,res)=>{
+      const text="<h1>Bienvenue</h1>"
+      res.send(text)
+  })
+
+  app.get('/api/persons',(req,res)=>{
+      res.json(Persons)
+  })
+
+  app.get('/api/persons/info',(req,res)=>{
+    let text=`<p>it ${Persons.length} people</p>`
+    let d=`<p>new Date()</p>`
+    res.send(` ${text}  ${d} `)
+
+  })
+
+  app.get('/api/persons/:id',(req,res)=>{
+      const id=Number(req.params.id)
+      const person=Persons.find(item=>item.id === id)
+      if(person){
+          res.send({number:person.number})
+      }
+      else{
+          res.status(404).end()
+      }
+      
+  })
+
+  app.delete('/api/persons/:id',(req,res)=>{
+    const id=Number(req.params.id)
+    Persons=Persons.filter(item=>item.id !==id)
+    res.status(204).end()
+  })
+
+  app.post('/api/persons',(req,res)=>{
+
+      let body=req.body
+
+      console.log('bod',body)
+
+    if(!body.name || !body.number){
+            res.status(400).json({err:'nom || numero  non renseigne'})
+      }
+
+    let num=Persons.find(item=>item.name ===body.name)
+
+    if(num){
+        res.status(400).json({ error: 'name must be unique' })
+    }
+      
+    let obj={
+          id: getRandomInt(),
+          name:body.name,
+          number:body.number
+      }
+      Persons=Persons.concat(obj)
+      res.json(obj)
+
+  })
+const PORT=3001
+app.listen(PORT,()=>console.log('Run on port',PORT)) 
